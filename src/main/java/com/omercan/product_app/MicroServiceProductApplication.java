916c829200abb36f56c0603f5bb41ad11aaa66f7
Ömercan Aras <canaras56789@gmail.com>
2024-05-09 13:17:04 +0200

    initial commit

--- src/main/java/com/omercan/product_app/MicroServiceProductApplication.java
+++ src/main/java/com/omercan/product_app/MicroServiceProductApplication.java
@@ -5,13 +5,18 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
-
+//Development profilinde hangi ayar dosyasının baz alınacağını belirtiyor
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 
 public class MicroServiceProductApplication {
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args)
+
+	{
 		SpringApplication.run(MicroServiceProductApplication.class, args);
 	}
 
